146cef154d31234dc29c693c83f3a5cab2580462
kevinvnguyen97 <kevinvannguyen1997@gmail.com>
2024-12-23 12:28:14 -0600

    initial ui, home page

---
+++ src/components/custom/GameSearch.tsx
@@ -0,0 +1,22 @@
+import { Group, Input, IconButton } from "@chakra-ui/react";
+import { useState } from "react";
+import { AiOutlineSearch } from "react-icons/ai";
+
+export const GameSearch = () => {
+  const [input, setInput] = useState("");
+
+  return (
+    <Group width="100%" maxWidth={500} attached>
+      <Input
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+        placeholder="Search for a game"
+        variant="subtle"
+        size="xl"
+      />
+      <IconButton size="xl" colorPalette="blue">
+        <AiOutlineSearch />
+      </IconButton>
+    </Group>
+  );
+};
