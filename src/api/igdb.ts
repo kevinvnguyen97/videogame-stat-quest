453e8b379d65b76c0920daeb0f78253d0e90fa6f
kevinvnguyen97 <kevinvannguyen1997@gmail.com>
2025-01-13 17:18:40 -0600

    break down id list to multiples if url is too long

--- src/api/igdb.ts
+++ src/api/igdb.ts
@@ -1,7 +1,10 @@
+import ArraySlice from "lodash";
+
 const BASE_URL = "http://localhost:3000/api";
 
 const API_WAIT_TIME_MS = 1000;
 const API_RETRY_LIMIT = 20;
+const MAX_ID_CHUNKS = 200;
 
 export enum IGDBEndpoint {
   GAMES = "games",
@@ -30,27 +33,37 @@ export async function getIGDBRecords<T>(args: {
   }
   let retryLimit = API_RETRY_LIMIT;
   let records: T[] = [];
-  const url = `${BASE_URL}/${endpoint}${
-    ids.length > 0 ? `/${filteredIds.join(",")}` : ""
-  }?fields=*${search ? `&search=${search}` : ""}&limit=500`;
-  const encodedUrl = encodeURI(url);
-  do {
-    const response = await fetch(encodedUrl, {
-      method: "POST",
-      headers: {
-        Accept: "application/json",
-        "Client-ID": import.meta.env.VITE_IGDB_CLIENT_ID,
-        Authorization: `Bearer ${import.meta.env.VITE_IGDB_ACCESS_TOKEN}`,
-      },
-    });
-    // Retry
-    if (response.status === 429) {
-      retryLimit--;
-      setTimeout(() => {}, API_WAIT_TIME_MS);
-      continue;
-    }
-    records = (await response.json()) || [];
-    break;
-  } while (retryLimit > 0);
+  const chunkedIds =
+    search || filteredIds.length > 0
+      ? ArraySlice.chunk(filteredIds, MAX_ID_CHUNKS)
+      : [[]];
+
+  for (let i = 0; i < (chunkedIds.length || 1); i++) {
+    const idList =
+      search || chunkedIds[i].length > 0 ? chunkedIds[i] || [] : [];
+    const url = `${BASE_URL}/${endpoint}${
+      idList.length > 0 && !search ? `/${idList.join(",")}` : ""
+    }?fields=*${search ? `&search=${search}` : ""}&limit=500`;
+    const encodedUrl = encodeURI(url);
+    do {
+      const response = await fetch(encodedUrl, {
+        method: "POST",
+        headers: {
+          Accept: "application/json",
+          "Client-ID": import.meta.env.VITE_IGDB_CLIENT_ID,
+          Authorization: `Bearer ${import.meta.env.VITE_IGDB_ACCESS_TOKEN}`,
+        },
+      });
+      // Retry
+      if (response.status === 429) {
+        retryLimit--;
+        setTimeout(() => {}, API_WAIT_TIME_MS);
+        continue;
+      }
+      records = [...records, ...((await response.json()) || [])];
+      break;
+    } while (retryLimit > 0);
+  }
+
   return records;
 }
